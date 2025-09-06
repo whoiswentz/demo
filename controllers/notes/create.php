64a0a221f12c27edf69ae38df4bca9fafc60a8d0
Vinicios Wentz <wentz.vinicios@gmail.com>
2025-09-05 21:49:25 -0300

    simple form validation

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -8,11 +8,22 @@ $config = require 'config.php';
 
 $db = new Database($config['database'], $username, $password);
 
+$errors = [];
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query("insert notes (body, user_id) values (:body, :user_id)", [
-        'body' => $_POST['body'],
-        'user_id' => 1,
-    ]);
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'Body cannot be longer than 1000 characters';
+    }
+
+    if (empty($errors)) {
+        $db->query("insert notes (body, user_id) values (:body, :user_id)", [
+            'body' => $_POST['body'],
+            'user_id' => 1,
+        ]);
+    }
 }
 
 include "views/note-create.view.php";
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -15,8 +15,13 @@
                         name="body" 
                         rows="4" 
                         class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        placeholder="Write your note here...">
+                        placeholder="Write your note here..."
+                        required>
+                        <?= htmlspecialchars($_POST['body'] ?? '') ?>
                     </textarea>
+                    <?php if (isset($errors['body'])) : ?>
+                        <p class="text-sm text-red-600 mt-4"><?= $errors['body'] ?></p>
+                    <?php endif; ?>
                 </div>
             </div>
             <div class="flex items-center justify-end gap-x-6">
